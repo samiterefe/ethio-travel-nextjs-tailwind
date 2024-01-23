99c62f9d8c37c6daa4ee8dfd239c6e375c8d786b
samiterefe <samuelterefe2@gmail.com>
2024-01-23 08:55:52 -0500

    almost finishing

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import Camp from "@/components/Sofumer";
 import HomeHero from "@/components/HomeHero";
 import Image from "next/image";
+import Guide from "@/components/Guide";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
 
 export default function Home() {
   return (
-    <HomeHero/>
+    <>
+     <HomeHero/>
+     <Camp/>
+     <Guide />
+      <Features />
+      <GetApp />
+    </>
+
+   
   );
 }
